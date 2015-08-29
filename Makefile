4a4db95d2c26bfd04edef2bcd865cb5ce210fc98
Martin Jambon <martin@mjambon.com>
2015-08-29 02:20:47 -0700

    First implementation and interface of the Trax module which attaches trace information to exceptions.

---
+++ Makefile
@@ -0,0 +1,48 @@
+VERSION = 0.0.1
+export VERSION
+
+NATDYNLINK := $(shell if [ -f `ocamlfind ocamlc -where`/dynlink.cmxa ]; then \
+                         echo YES; else echo NO; \
+                      fi)
+
+ifeq "${NATDYNLINK}" "YES"
+CMXS=trax.cmxs
+endif
+
+.PHONY: default all opt test doc soft-clean clean
+default: all opt
+all:
+	ocamlfind ocamlc -c trax.mli
+	ocamlfind ocamlc -c -dtypes trax.ml
+	touch bytecode
+
+opt: trax.cmx $(CMXS)
+	touch nativecode
+
+trax.cmx:
+	ocamlfind ocamlc -c trax.mli
+	ocamlfind ocamlopt -c -dtypes trax.ml
+
+trax.cmxs: trax.cmx
+	ocamlfind ocamlopt -I . -shared -linkall -o trax.cmxs trax.cmx
+
+clean:
+	rm -f *.cm[iox] *.cmxs *.o *.annot bytecode nativecode
+
+COMMON_INSTALL_FILES = META trax.cmi trax.mli
+BC_INSTALL_FILES = trax.cmo
+NC_INSTALL_FILES = trax.cmx trax.o $(CMXS)
+
+install:
+	echo "version = \"$(VERSION)\"" > META; cat META.tpl >> META
+	INSTALL_FILES="$(COMMON_INSTALL_FILES)"; \
+		if test -f bytecode; then \
+		  INSTALL_FILES="$$INSTALL_FILES $(BC_INSTALL_FILES)"; \
+		fi; \
+		if test -f nativecode; then \
+		  INSTALL_FILES="$$INSTALL_FILES $(NC_INSTALL_FILES)"; \
+		fi; \
+		ocamlfind install trax $$INSTALL_FILES
+
+uninstall:
+	ocamlfind remove trax
---
+++ trax.ml
@@ -0,0 +1,55 @@
+open Printf
+
+type location = Text of string
+              | Raw_backtrace of Printexc.raw_backtrace
+
+exception Traced of exn * location list
+
+let wrap loc e =
+  match e with
+  | Traced (e, locs) -> Traced (e, loc :: locs)
+  | e -> Traced (e, [loc])
+
+let unwrap = function
+  | Traced (e, _) -> e
+  | e -> e
+
+let wrap_with_backtrace e =
+  wrap (Raw_backtrace (Printexc.get_raw_backtrace ())) e
+
+let raise_at loc e =
+  Pervasives.raise (wrap loc e)
+
+let raise txt_loc e =
+  raise_at (Text txt_loc) e
+
+let reraise_with_backtrace e =
+  raise_at (Raw_backtrace (Printexc.get_raw_backtrace ())) e
+
+let add_loc buf loc =
+  match loc with
+  | Text s ->
+      bprintf buf "\n%s" s
+  | Raw_backtrace x ->
+      let s = Printexc.raw_backtrace_to_string x in
+      let len = String.length s in
+      if len > 0 then
+        let n =
+          if s.[len-1] = '\n' then len - 1
+          else len
+        in
+        Buffer.add_char buf '\n';
+        Buffer.add_substring buf s 0 n
+
+let to_string e =
+  match e with
+  | Traced (e, locs) ->
+      let buf = Buffer.create 500 in
+      bprintf buf "%s" (Printexc.to_string e);
+      List.iter (add_loc buf) (List.rev locs);
+      Buffer.contents buf
+  | e ->
+      Printexc.to_string e
+
+let print oc e =
+  output_string oc (to_string e)
